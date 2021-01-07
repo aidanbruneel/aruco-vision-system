a920c4aa17b7bbaca0f29b49b7f7f38d865c1f0e
Aidan Bruneel <abruneel@uwo.ca>
2021-01-07 05:49:19 -0500

    Camera Calibration: - createKnownBoardPosition() added - getChessboardCorners() added

--- hello-world/Source.cpp
+++ hello-world/Source.cpp
@@ -14,6 +14,37 @@ using namespace cv;
 
 const float calibrationSquareDimension = 0.02425f; //mm
 const float arucoSquareDimension = 0.2000f; // NOT MEASURED
+const Size chessboardDimensions = Size(9, 6);
+
+void createKnownBoardPosition(Size boardSize, float squareEdgeLength, vector<Point3f>& corners)
+{
+	for (int i = 0; i < boardSize.height; i++)
+	{
+		for (int j = 0; j < boardSize.width; j++)
+		{
+			corners.push_back(Point3f(j * squareEdgeLength, i * squareEdgeLength, 0.0f));
+		}
+	}
+}
+
+void getChessboardCorners(vector<Mat> images, vector<vector<Point2f>>& allFoundCorners, bool showResults = false)
+{
+	for (vector<Mat>::iterator iter = images.begin(); iter != images.end(); iter++)
+	{
+		vector<Point2f> pointBuf;
+		bool found = findChessboardCorners(*iter, Size(9, 6), pointBuf, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE);
+		if (found)
+		{
+			allFoundCorners.push_back(pointBuf);
+		}
+		if (showResults)
+		{
+			drawChessboardCorners(*iter, Size(9, 6), pointBuf, found);
+			imshow("Looking for Corners", *iter);
+			waitKey(0);
+		}
+	}
+}
 
 void createArucoMarkers()
 {
@@ -76,5 +107,36 @@ int scanCamera()
 
 int main(int argv, char* argc)
 {
-	scanCamera();
+	Mat frame;
+	Mat drawToFrame;
+
+	Mat cameraMatrix = Mat::eye(3, 3, CV_64F);
+	Mat distanceCoefficients;
+	vector<Mat> savedImages;
+	vector<vector<Point2f>> markerCorners, rejectedCandidates;
+	
+	VideoCapture cap = cameraSetup();
+	if (!cap.isOpened())
+	{
+		return 0;
+	}
+	
+	int framesPerSecond = 20;
+	namedWindow("Webcam", WINDOW_AUTOSIZE);
+
+	while (true)
+	{
+		if (!cap.read(frame)) break;
+
+		vector<Vec2f> foundPoints;
+		bool found = false;
+		found = findChessboardCorners(frame, chessboardDimensions, foundPoints, CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_NORMALIZE_IMAGE);
+		
+		frame.copyTo(drawToFrame);
+		drawChessboardCorners(drawToFrame, chessboardDimensions, foundPoints, found);
+		if (found) imshow("Webcam", drawToFrame);
+		else imshow("Webcam", frame);
+		char character = waitKey(1000 / framesPerSecond);
+	}
+	return 0;
 }
